d62def853232064d59570a06c1dc123ed9e089b6
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 08:16:00 -0300

    Update Desafio 4.py

--- Desafio 4.py
+++ Desafio 4.py
@@ -1,8 +1,10 @@
 #CALCULADORA
+#'n1' e 'n2' são as variaveis criadas para selecionar os numeros
 n1 = float(input("escolha um numero"))
 n2 = float(input("escolha outro numero"))
+#'operação' é a variavel para selecionar a operação usada
 operação = input("escolha a operação:[soma, subtração, multiplicação, divisão]")
-
+#'if' é para colocar 
 if operação == "soma":
     print(n1+n2)
 elif operação == "subtração":
