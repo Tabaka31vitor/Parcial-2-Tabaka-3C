e18b07acb57b2a358de7a11f14cb2f151ed67af8
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 08:23:04 -0300

    Add comments to triangle area calculation
    Added comments to explain the triangle area calculation.

---
+++ Desafio 5.py
@@ -0,0 +1,9 @@
+#AREA DO TRIANGULO
+#'base' e 'altura' foram as variaveis criadas para fazer as perguntas de base e altura do triangulo
+base = float(input("qual a base do triangulo?"))
+altura = float(input("qual a altura do triangulo?"))
+
+#com base nas respostas das variaveis acima foi colocada a fórmula da area do triangulo para ser resolvido segundo a resposta da base e altura
+area = (base * altura) / 2
+#'print' é para mostrar a resposta do calculo da formula do triangulo
+print(area)
