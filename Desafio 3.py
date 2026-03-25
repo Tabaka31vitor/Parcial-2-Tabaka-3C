56545aa5d52b6e03171d2efb5492872cf1735476
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 13:45:44 -0300

    Implement user input for objects, movies, games, food, and sports
    Added user input prompts for various categories and displayed the final list.

--- Desafio 3.py
+++ Desafio 3.py
@@ -1,5 +1,12 @@
 #LISTA DE NOMES
-#'objetos' é a variável usada para criar a lista
-objetos = "porta","madeira","maçaneta","trinco","dobradiça"
-#'print' foi usada para mostrar ao usuario a lista criada pela variável 'objetos'
-print(objetos)
+#foi feito uma variável para cada pergunta
+obj = input("escolha um objeto: ")
+filme = input("escolha um filme: ")
+jogo = input("escolha um jogo: ")
+comida = input("escolha uma comida: ")
+esporte = input("escolha um esporte: ")
+#coloquei um 'print()' só para ficar um espaço das perguntas para a lista
+print()
+#aqui é a resposta conforme a resposta de cada pergunta feita
+print("Sua lista esta pronta!")
+print(obj,  filme,  jogo,  comida,  esporte)
