7f9525520b5e25ac8a4eeba4c1ba230f56db5c39
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 08:40:28 -0300

    Update and rename desafio 7 to desafio 7.py

---
+++ desafio 7.py
@@ -0,0 +1,11 @@
+#TAXA DE JUROS
+#'c' é a variavel para o valor da capital
+c = float(input("digite o valor de capital"))
+#'i' é a variavel para o valor da taxa 
+i = float(input("digite o valor de i"))
+#'t' é a variavel para o valor do tempo
+t = float(input("digite o valor de t"))
+
+#'j' é uma variavel criada
+j = (c*i*t) /100
+print(j)
