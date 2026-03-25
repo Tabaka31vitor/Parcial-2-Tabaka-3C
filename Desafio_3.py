de5613a3b5c776cb20df1fde7284526c51a92d96
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 08:09:15 -0300

    Add comments to Desafio_3.py for clarity
    Added comments to explain the 'objetos' variable and print function.

---
+++ Desafio_3.py
@@ -0,0 +1,5 @@
+#LISTA DE NOMES
+#'objetos' é a variável usada para criar a lista
+objetos = "porta","madeira","maçaneta","trinco","dobradiça"
+#'print' foi usada para mostrar ao usuario a lista criada pela variável 'objetos'
+print(objetos)
