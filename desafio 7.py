9b0152dd23515928748547966b668fdf35f1d37f
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 08:44:06 -0300

    Update desafio 7.py

--- desafio 7.py
+++ desafio 7.py
@@ -6,6 +6,7 @@ i = float(input("digite o valor de i"))
 #'t' é a variavel para o valor do tempo
 t = float(input("digite o valor de t"))
 
-#'j' é uma variavel criada
+#'j' é uma variavel criada para conseguir ser impressa a resposta depois. E também foi criada para conseguir fazer o resultado da fórmula dos juros
 j = (c*i*t) /100
+#print é só para mostrar a resposta para o usuario
 print(j)
