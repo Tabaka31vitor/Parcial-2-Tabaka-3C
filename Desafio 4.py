6a70ba2490c365cd11dca977dafc3f1e5ee846b5
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 08:35:04 -0300

    Fix spelling of operations in calculator script

--- Desafio 4.py
+++ Desafio 4.py
@@ -3,18 +3,18 @@
 n1 = float(input("escolha um numero"))
 n2 = float(input("escolha outro numero"))
 #'operação' é a variavel para selecionar a operação usada
-operação = input("escolha a operação:[soma, subtração, multiplicação, divisão]")
+operação = input("escolha a operação:[soma, subtracao, multiplicacao, divisao]")
 #'if' é para colocar as opções da escolha da operação. Então se for 'soma' realizará a primeira opção
 if operação == "soma":
     print(n1+n2)
 #'elif' foi usado para "se não for soma, então pode ser subtração"
-elif operação == "subtração":
+elif operação == "subtracao":
     print(n1-n2)
 #'elif' foi usado para "se não for soma e nem subtração, então pode ser multiplicação"
-elif operação == "multiplicação":
+elif operação == "multiplicacao":
     print(n1*n2)
 #'elif' foi usado para "se não for soma, nem subtração e nem multiplicação, então pode ser divisão"
-elif operação == "divisão":
+elif operação == "divisao":
     print(n1/n2)
 #'else' foi usado para caso nao seja nenhuma das operações descritas então dará a resposta "esse numero nao existe"
 else:
