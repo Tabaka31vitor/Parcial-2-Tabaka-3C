4c3d4865f0c6828709479807ab77cd228f5069b7
Vitor Tabaka <147004715+Tabaka31vitor@users.noreply.github.com>
2026-03-25 08:20:34 -0300

    Update Desafio 4.py

--- Desafio 4.py
+++ Desafio 4.py
@@ -4,14 +4,18 @@ n1 = float(input("escolha um numero"))
 n2 = float(input("escolha outro numero"))
 #'operação' é a variavel para selecionar a operação usada
 operação = input("escolha a operação:[soma, subtração, multiplicação, divisão]")
-#'if' é para colocar 
+#'if' é para colocar as opções da escolha da operação. Então se for 'soma' realizará a primeira opção
 if operação == "soma":
     print(n1+n2)
+#'elif' foi usado para "se não for soma, então pode ser subtração"
 elif operação == "subtração":
     print(n1-n2)
+#'elif' foi usado para "se não for soma e nem subtração, então pode ser multiplicação"
 elif operação == "multiplicação":
     print(n1*n2)
+#'elif' foi usado para "se não for soma, nem subtração e nem multiplicação, então pode ser divisão"
 elif operação == "divisão":
     print(n1/n2)
+#'else' foi usado para caso nao seja nenhuma das operações descritas então dará a resposta "esse numero nao existe"
 else:
     print("esse numero nao existe")
